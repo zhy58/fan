b17b492f5000362bdd445f33136ddb134f047347
zhy58 <1139733050@qq.com>
2020-02-02 21:05:17 +0800

    edit package name

--- android/app/src/main/java/com/zhy/fan/ble/AdvertiserPackage.java
+++ android/app/src/main/java/com/zhy/fan/ble/AdvertiserPackage.java
@@ -1,4 +1,4 @@
-package com.fan.ble;
+package com.zhy.fan.ble;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
--- android/app/src/main/java/com/zhy/fan/ble/SqlModel.java
+++ android/app/src/main/java/com/zhy/fan/ble/SqlModel.java
@@ -1,4 +1,4 @@
-package com.fan.ble;
+package com.zhy.fan.ble;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
